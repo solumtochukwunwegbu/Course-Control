3d050a2fbed7162592604feb3bec6dad0a72222d
solumtochukwunwegbu <solumtochukwunwegbu@gmail.com>
2025-05-14 18:19:04 +0100

    modified

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-// Import the sql module
-const mysql = require('mysql2');
-
-
 // Import the express module
 const express = require('express');
 const app = express();
 const port = 3000; // You can change the port number
 
+
+// Import the sql module
+const mysql = require('mysql2');
+
+
 // Import the ejs module
 app.set("view engine","ejs");
+app.set('views', __dirname);
+
+//Import the bodyParser
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended : true}));
+
+
+// Serve the root folder as static
+app.use(express.static(__dirname));
+
 
 
 //===============================================
@@ -28,14 +39,47 @@ connection.connect(
   }
 );
 
-//===================================================
-// Basic route that responds with "Hello World"
-app.get('/', (req, res) => {
-  res.render("index");
-});
+
+
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:`+ port);
 });
+
+
+
+
+//==========================================================================================================
+//==========================================================================================================
+//==========================================================================================================
+//==========================================================================================================
+//==========================================================================================================
+
+
+
+app.get('/', (req, res) => {
+  const sql = 'SELECT * FROM course';
+
+  connection.query(sql, (err, results) => {
+    if (err) throw err;
+
+    res.render('index', { course: results });
+  });
+});
+
+app.get('/courses', function (req, res) {
+  connection.query('SELECT * FROM course', function (err, results) {
+    if (err) throw err;
+    res.render('courses', { course: results });
+  });
+});
+
+app.get('/profile', function (req, res) {
+  connection.query('SELECT * FROM course', function (err, results) {
+    if (err) throw err;
+    res.render('profile', {profile: results });
+  });
+});
+
 
---
+++ query.sql
@@ -0,0 +1,10 @@
+UPDATE course SET  background_img = 'https://th.bing.com/th/id/OIP.NWa23EB5gvquBIjIQKYKVQHaEE?rs=1&pid=ImgDetMain' WHERE title = 'Coding';
+UPDATE course SET  background_img = 'https://digitaldefynd.com/wp-content/uploads/2020/02/Best-mechanical-engineering-course-tutorial-class-certification-training-online-scaled.jpg' WHERE title = 'Mechanics';
+UPDATE course SET  background_img = 'https://th.bing.com/th/id/OIP.SiqozW3FdPpDVDQeoUpszwHaEK?cb=iwc2&rs=1&pid=ImgDetMain' WHERE title = 'Economics';
+UPDATE course SET  background_img = 'https://www.adorama.com/alc/wp-content/uploads/2017/12/shutterstock_390834172.jpg' WHERE title = 'Photography';
+UPDATE course SET  background_img = 'https://th.bing.com/th/id/R.ab7306db6638cb2fbe54dcb17145f257?rik=JtEfwq9mhtybrg&pid=ImgRaw&r=0' WHERE title = 'Public Speaking';
+UPDATE course SET  background_img = 'https://img.freepik.com/premium-photo/innovating-light-bulb-ideas-sell-products-money_962751-5057.jpg' WHERE title = 'Entrepreneurship';
+UPDATE course SET  background_img = 'https://blog.coffeelunchcoffee.com/wp-content/uploads/2021/10/Chart-Your-Course-750x410.jpg' WHERE title = 'History';
+
+
+SELECT * FROM course;
